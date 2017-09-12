87fe2d81d876e2b7fe296d7311986e39873f5abd
Dariell Vasquez <dariell@Dariells-Air.fios-router.home>
2017-09-11 22:03:34 -0400

    Finished Most of game logic

--- assets/javascript/crystals.js
+++ assets/javascript/crystals.js
@@ -1,46 +1,101 @@
 var targetNumber;
 var numberOptions;
-var counter = 0;
+var counterTotal = 0;
+$("#counter-total").text(counterTotal);
+var wins = 0;
+$("#wins").text(wins);
+var losses = 0;
+$("#losses").text(losses);
+var numberOptionsArr = [];
+numberOptionsArr = [numberOptions(), numberOptions(), numberOptions(), numberOptions()];
 
+function numberOptions() {
+    var randomNumber = Math.floor(Math.random() * 12) + 1;
+    return randomNumber;
+}
+
+function randomOneThruThree() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    return randomNumber;
+}
 
 $(document).ready(function() {
     targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
     $("#current-random-num").text(targetNumber);
 
-    numberOptions = Math.floor(Math.random() * 12) + 1;
 
-    for (var i = 0; i < numberOptions.length; i++) {
-        // For each iteration, we will create an imageCrystal
-        var imageCrystal = $("<img>");
 
-        // Each imageCrystal will be given a data attribute called data-crystalValue.
-        // This data attribute will be set equal to the array value.
-        imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-        // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-        $("#crystals").append(imageCrystal);
+    function restart() {
+        targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+
+        numberOptions();
+        numberOptionsArr = [numberOptions(), numberOptions(), numberOptions(), numberOptions()];
+    }
+
+    function randomOneThruFour() {
+        var randomNumber = Math.floor(Math.random() * 4) + 1;
+        return randomNumber;
     }
 
 
+    var crystalOne = $("#crystal-1");
+    var crystalTwo = $("#crystal-2");
+    var crystalThree = $("#crystal-3");
+    var crystalFour = $("#crystal-4");
 
-    // This time, our click event applies to every single crystal on the page. Not just one.
+
+
+    $(crystalOne).attr("data-crystalvalue", numberOptionsArr[randomOneThruThree()]);
+    $(crystalTwo).attr("data-crystalvalue", numberOptionsArr[randomOneThruThree()]);
+    $(crystalThree).attr("data-crystalvalue", numberOptionsArr[randomOneThruThree()]);
+    $(crystalFour).attr("data-crystalvalue", numberOptionsArr[randomOneThruThree()]);
+
+
+    // while (first === second || first === third || first === fourth) {
+    //     numberOptionsArr[randomOneThruThree()];
+    //     console.log(first);
+    // } else if (second === first || second === third || second === fourth) {
+    //     first = numberOptionsArr[randomOneThruThree()];
+    // } else if (third === first || third === second || third === fourth) {
+    //     first = numberOptionsArr[randomOneThruThree()];
+    // } else if (fourth === first || fourth === second || fourth === third) {
+    //     first = numberOptionsArr[randomOneThruThree()];
+    // } else {
+
+
+// Fix alert when lose 
+
+// * Get the numbers to have their own individual counter **
+
+
     $(".crystal-image").on("click", function() {
-        // Determining the crystal's value requires us to extract the value from the data attribute.
-        // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-        // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-        // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
 
         var crystalValue = ($(this).attr("data-crystalvalue"));
         crystalValue = parseInt(crystalValue);
-        // We then add the crystalValue to the user's "counter" which is a global variable.
+        console.log(crystalValue);
+        // Add the crystalValue to the user's "counter" which is a global variable.
         // Every click, from every crystal adds to the global counter.
-        counter += crystalValue;
-        // All of the same game win-lose logic applies. So the rest remains unchanged.
-        alert("New score: " + counter);
-        if (counter === targetNumber) {
+        counterTotal += crystalValue;
+        $("#counter-total").text(counterTotal);
+
+        if (counterTotal === targetNumber) {
             alert("You win!");
-        } else if (counter >= targetNumber) {
+            wins++;
+            $("#wins").text(wins);
+            restart();
+
+        } else if (counterTotal >= targetNumber) {
             alert("You lose!!");
+            losses++;
+            $("#losses").text(losses);
+            restart();
         }
     });
 
 });
+
+
+
+
+
+// $("#crystal-1").attr("data-crystalvalue", numberOptionsArr[i]);
